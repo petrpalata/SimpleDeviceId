edcfaee958c273b409774847ecd913c52267bc36
Petr Palata <petr.palata@gmail.com>
2022-02-05 12:59:30 +0100

    Add basic project structure for the framework

---
+++ simpledeviceid/simpledeviceid/simpledeviceid.h
@@ -0,0 +1,18 @@
+//
+//  SimpleDeviceId.h
+//  SimpleDeviceId
+//
+//  Created by Petr Palata on 05.02.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleDeviceId.
+FOUNDATION_EXPORT double SimpleDeviceIdVersionNumber;
+
+//! Project version string for SimpleDeviceId.
+FOUNDATION_EXPORT const unsigned char SimpleDeviceIdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleDeviceId/PublicHeader.h>
+
+
